72ff1f9f0bbec5ca7732885b781376092c2ea5ce
alexsouchereau <W0632604@myscc.ca>
2022-10-05 18:33:05 -0400

    added animations
    everything working, now converting jQuery AJAX to play javascript

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // WEB303 Assignment 2
 $(document).ready(function () {
     $("#prospect").click(function (e) { 
-        $("#content").load('prospect.html').css("display", "block");
+        let content = $("#content");
+        content.hide().text("");
+        content.load('prospect.html').fadeIn(600);
     });
 
     $("#convert").click(function (e) {
-        $("#content").load('convert.html').css("display", "block");
+        let content = $("#content");
+        content.hide().text("");
+        content.load('convert.html').fadeIn(600);
     });
 
     $("#retain").click(function (e) {
-        $("#content").load('retain.html').css("display", "block");
+        let content = $("#content");
+        content.hide().text("");
+        content.load('retain.html').fadeIn(600);
     });
 });
