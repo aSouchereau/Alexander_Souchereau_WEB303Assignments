1e733c111720465740a3a3128cc4a052e45461fe
alexsouchereau <W0632604@myscc.ca>
2022-10-05 16:52:49 -0400

    added link functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 // WEB303 Assignment 2
+$(document).ready(function () {
+    $("#prospect").click(function (e) { 
+        $("#content").load('prospect.html').css("display", "block");
+    });
 
+    $("#convert").click(function (e) {
+        $("#content").load('convert.html').css("display", "block");
+    });
 
+    $("#retain").click(function (e) {
+        $("#content").load('retain.html').css("display", "block");
+    });
+});
