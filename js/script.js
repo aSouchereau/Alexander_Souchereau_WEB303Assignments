5cc8ed0380f8c490a73933f9f78e733a35e24876
aSouchereau <98470000+aSouchereau@users.noreply.github.com>
2022-10-20 23:32:30 -0400

    Completed assignment

--- js/script.js
+++ js/script.js
@@ -4,11 +4,49 @@
 */
 
 $(function () {
-    // your code here
+   
+    if (!navigator.geolocation)
+    {
+        $('#content').prepend("<b>Please allow location services on this page...</b>");
+        console.log("not enabled");
+    }
+    else
+    {
+        console.log("Getting user location...");
+        navigator.geolocation.getCurrentPosition(success, fail);
+    }
+
+    function success(position) {
+        console.log(position);
+        let lat = parseFloat(position.coords.latitude);
+        let long = parseFloat(position.coords.longitude);
+        $("div#locationhere").text("Current Location: " + lat + " / " + long + " Accuracy: " + position.coords.accuracy);
+
+        if (localStorage.getItem("position"))
+        {
+            let storedPosition = localStorage.getItem("position");
+            console.log(storedPosition);
+            $('#content').append("Previous Location: " + storedPosition);
+
+            let storedPosArray = storedPosition.split(" / ");
+            let oldLat = parseFloat(storedPosArray[0]);
+            let oldLong = parseFloat(storedPosArray[1]);
+            let distance = (calcDistanceBetweenPoints(lat, long, oldLat, oldLong) / 1000).toFixed(4);
+            $('#content').append("<p>You travelled " + distance + "km since your last visit.");
+
+            localStorage.setItem("position", lat + " / " + long);
+        }
+        else
+        {
+            localStorage.setItem("position", lat + " / " + long);
+        }
+    }
 
 
 
-
+    function fail() {
+        console.log("Error getting position");
+    }
 
     // DO NOT EDIT ANY CODE IN THIS FUNCTION DEFINTION
     // function to calculate the distance in metres between two lat/long pairs on Earth
