fefe6624bb8e0d956dddead69f1b05070f75dbd0
alexsouchereau <W0632604@myscc.ca>
2022-11-11 23:01:00 -0500

    Completed assignment

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 $(function () {
     $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+        var $heading = $('body').clone().find('h1');
         var $content = $(this).clone().find('img').css({
             marginLeft: 0,
             marginTop: 0,
             width: '100%',
             height: 'auto'
         });
-        //modal code goes here
+
+        modal.open({heading:$heading, content:$content,  width:800, height:575});
     });
 });
