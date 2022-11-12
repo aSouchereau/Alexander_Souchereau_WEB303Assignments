4da0ae72ee4f4c94150de12e75bc7db3e500a5a1
alexsouchereau <W0632604@myscc.ca>
2022-11-11 19:04:40 -0500

    Added photo viewer

--- js/script.js
+++ js/script.js
@@ -1,31 +1,11 @@
-$(document).ready(function () {
-
-    $('.accordion').on('click', '.accordion-control', function(e) {
-        $(this)
-            .next('.accordion-panel')
-            .not(':animated')
-            .slideToggle();
-    });
-
-
-
-    $('.tab-list').each(function () {
-        var $this = $(this);
-        var $tab = $this.find('li.active');
-        var $link = $tab.find('a');
-        var $panel = $($link.attr('href'));
-
-        $this.on('click', '.tab-control', function (e) {
-            e.preventDefault();
-            var $link = $(this);
-            var id = this.hash;
-
-            if (id && !$link.parent().is('.active')) {
-                $panel.removeClass('active');
-                $tab.removeClass('active');
-                $panel = $(id).addClass('active');
-                $tab = $link.parent().addClass('active');
-            }
+$(function () {
+    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
+        var $content = $(this).clone().find('img').css({
+            marginLeft: 0,
+            marginTop: 0,
+            width: '100%',
+            height: 'auto'
         });
+        //modal code goes here
     });
 });
