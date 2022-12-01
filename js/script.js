35c6b8adb251ae2e654f3b846bc011c829e68f9b
alexsouchereau <W0632604@myscc.ca>
2022-11-30 22:00:01 -0500

    Completed assignment

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function (){
+    let $form = $('form');
+    let $submitBtn = $('input[type="submit"]');
+
+    $submitBtn.prop('disabled', true);
+    let errorMsg = [];
+
 
     //Populate countries dropdown list
     $(countries).each(function () {
@@ -6,5 +12,72 @@ $(document).ready(function (){
         $('select#country').append(element);
     });
 
-console.log(countries);
+    // On input, check if form is able to be submitted
+    $form.on('input', function () {
+        if (validateUsername() && validatePassword() && confirmPassword() && validateTerms() && validateCountry()) {
+            $submitBtn.prop('disabled', false);
+        }
+        else {
+            console.log("Invalid entries");
+        }
+    });
+
+    // Intercept submit action and display message
+    $form.on('submit', function (e) {
+       e.preventDefault();
+       $form.find('.successMsg').empty();
+       let successMessage = '<div class="successMsg"><p>Welcome ' +
+           $('#username').val() +
+           '! The country code you selected is ' +
+           $('#country').val() + '</p></div>'
+       $form.append(successMessage);
+       console.log("Sign up successful...");
+    });
+
+    // Username validation
+    function validateUsername() {
+        if ($('#username').val()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Password validation
+    function validatePassword() {
+        if ($('#password').val().length >= 12) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Password confirmation {
+    function confirmPassword() {
+        if ($('#password').val() == $('#confirmPassword').val()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Terms Validation
+    function validateTerms() {
+        if ($('#terms').is(':checked')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Country Validations
+    function validateCountry() {
+        if ($('#country').val() && $('#country').val() != "unselected") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 });
